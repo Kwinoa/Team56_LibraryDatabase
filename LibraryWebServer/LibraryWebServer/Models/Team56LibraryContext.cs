cca06bd627c7373645fd3a09b6c9cd399fc0bbe6
SINAHI LEON <Sinahi.Leon1@gmail.com>
2026-03-19 16:10:01 -0600

    changed password to pwd

--- LibraryWebServer/LibraryWebServer/Models/Team56LibraryContext.cs
+++ LibraryWebServer/LibraryWebServer/Models/Team56LibraryContext.cs
@@ -27,7 +27,7 @@ public partial class Team56LibraryContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=atr.eng.utah.edu;user id=u1430000;password=u1430000;database=Team56Library", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.11.16-mariadb"));
+        => optionsBuilder.UseMySql("server=atr.eng.utah.edu;user id=u1430000;password=pwd;database=Team56Library", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.11.16-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
